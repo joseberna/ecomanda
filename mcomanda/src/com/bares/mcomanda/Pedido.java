36010357225083424e838bcf9410e6c3dc693bad
joseberna <josefbernam@gmail.com>
2014-06-02 23:14:31 -0500

    ok primer commit no funcional

---
+++ mcomanda/src/com/bares/mcomanda/Pedido.java
@@ -0,0 +1,5 @@
+package com.bares.mcomanda;
+
+public class Pedido {
+
+}
---
+++ mcomanda/src/com/bares/mcomanda/PedidosFragment.java
@@ -0,0 +1,7 @@
+package com.bares.mcomanda;
+
+import android.support.v4.app.Fragment;
+
+public class PedidosFragment extends Fragment {
+	
+}
