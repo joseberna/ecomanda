004a245ec79cc92e262afa50317ce47a32670ea8
joseberna <josefbernam@gmail.com>
2014-06-03 23:05:28 -0500

    new comanda

--- mcomanda/src/com/bares/mcomanda/Pedido.java
+++
@@ -1,5 +0,0 @@
-package com.bares.mcomanda;
-
-public class Pedido {
-
-}
--- mcomanda/src/com/bares/mcomanda/PedidosFragment.java
+++
@@ -1,7 +0,0 @@
-package com.bares.mcomanda;
-
-import android.support.v4.app.Fragment;
-
-public class PedidosFragment extends Fragment {
-	
-}
